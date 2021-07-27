9cde77e2441100a43132c94311c071176d0ec5aa
polatemre <41800395+polatemre@users.noreply.github.com>
2021-07-27 15:42:04 +0300

    Add pipe

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -12,23 +14,25 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
+  filterText = '';
 
   // ActivatedRoute: Built-in Angular servisi, mevcut route
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService
   ) {}
 
   // Component ilk açıldığında çalışan fonksiyon
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
-      if(params["categoryId"]){
-        this.getProductsByCategory(params["categoryId"])
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategory(params['categoryId']);
+      } else {
+        this.getProducts();
       }
-      else{
-        this.getProducts()
-      }
-    })
+    });
   }
 
   // Eğer yazdığımız servis ile ilgili component bazlı kurallar varsa subscribe'ı component'e geçiririz.
@@ -49,4 +53,12 @@ export class ProductComponent implements OnInit {
       });
   }
 
+  addToCart(product: Product) {
+    if (product.productId === 1) {
+      this.toastrService.error('Bu ürün sepete eklenemez', 'Hata');
+    } else {
+      this.toastrService.success(product.productName, 'Sepete eklendi');
+      this.cartService.addToCart(product);
+    }
+  }
 }
